4e4be3e5723c4a13d0043641c4407237e89e3b37
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-28 18:22:04 +0900

    update

--- cpp_utils.h
+++ cpp_utils.h
@@ -2,6 +2,32 @@
 #define _CPP_UTILS_H_
 
 #include <iostream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+/*create json object*/
+/*
+  json j;
+  j["pi"] = 3.141;
+  j["happy"] = true;
+  j["name"] = "Niels";
+  j["nothing"] = nullptr;
+  j["answer"]["everything"] = 42;  // 存在しないキーを指定するとobjectが構築される
+  j["list"] = { 1, 0, 2 };         // [1,0,2]
+  j["object"] = { {"currency", "USD"}, {"value", 42.99} };  // {"currentcy": "USD", "value": 42.99}
+*/
+
+json json_parse(std::string s, bool debug=false)
+{
+    json j = json::parse(s);
+
+    if (debug)
+      std::cout << j << std::endl;
+
+    return j;
+}
+
 
 std::string list2string(std::vector<std::string> list)
 {

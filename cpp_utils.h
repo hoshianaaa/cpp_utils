17190c0cc7adf0381b20f114da4e71e06098027b
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-28 12:56:37 +0900

    update

--- cpp_utils.h
+++ cpp_utils.h
@@ -1,3 +1,6 @@
+#ifndef _CPP_UTILS_H_
+#define _CPP_UTILS_H_
+
 #include <iostream>
 
 std::string list2string(std::vector<std::string> list)
@@ -12,3 +15,10 @@ std::string list2string(std::vector<std::string> list)
   }
   return ret;
 }
+
+double per2pix(double per)
+{
+return per / 100 * 255;
+}
+
+#endif
